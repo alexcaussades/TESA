07588154563129aa5036f3f7abd81e34ef340f72
Alexcaussades <alexcaussades@gmail.com.com>
2021-01-03 01:48:23 +0100

    add creatinvite and IA

--- apps.js
+++ apps.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("I am learning my functions",{type: "PLAYING"});
 
-  });
+});
 
 client.on("message", message =>{
     if (message.content === prefix + "devs"){
@@ -30,7 +30,7 @@ client.on("message", message =>{
     }else if(message.content === prefix + "help"){
         message.channel.send("Actuellement, mon module Help est absent.\n &feedback pour exécuter une demande");
     } else if(message.content === reqtesa + " hello"){
-       message.channel.send(`${message.author} Ta vue l'heure, je dors là ! Hey monsieur, je suis une ados qui a besoin de repos `);
+        message.channel.send(`${message.author} Ta vue l'heure, je dors là ! Hey monsieur, je suis une ados qui a besoin de repos `);
     } else if(message.content === reqtesa){
         message.channel.send(`${message.author} Qu'es que tu me veux ?`);
     }else if(message.content === reqtesa + " sois gentille"){
@@ -58,12 +58,12 @@ client.on("message",message => {
     if(message.content === prefix +"creat database"){
         message.channel.send("creation de la base en cour");
         database.creatdata;
-           message.channel.send("La création de la base de donées à bien étais éffectuer !");
-           message.channel.send(tesa.i).then( message => {message.channel.send(tesa.rp1), 3000});
-           message.channel.send("--------").then(message => {message.channel.send(tesa.rp2), 3000});
-       }
+        message.channel.send("La création de la base de donées à bien étais éffectuer !");
+        message.channel.send(tesa.i).then( message => {message.channel.send(tesa.rp1), 3000});
+        message.channel.send("--------").then(message => {message.channel.send(tesa.rp2), 3000});
+    }
 
-   });
+});
 
 
 client.on("message", message => {
@@ -79,7 +79,7 @@ client.on("message", message =>{
     const command = args.shift().toLowerCase();
     async function getUrlMetar(icao) {
         message.delete();
-       let sr =  process.env.metarivao+icao;
+        let sr =  process.env.metarivao+icao;
         request(sr, function (error, response, body) {
             const exampleEmbed = new Discord.MessageEmbed()
                 .setColor('#8a2be2')
@@ -109,69 +109,69 @@ client.on("message", message =>{
 })
 
 client.on("message", message =>{
-        const commandBody = message.content.slice(prefix.length);
-        const args = commandBody.split(' ');
-        const command = args.shift().toLowerCase();
-        if(command === "atc"){
-            let icao = args
-            if(!icao){
-                process.exit();
-            }
-            let sr = process.env.apiatc+icao;
-            request(sr, function (error, response, body) {
-                const obj = JSON.parse(body);
-
-                if(obj.nameAirport != undefined){
-                    message.channel.send("Information ATC on " +obj.nameAirport);
-                }
-
-                const data = obj.data
-                try {
-                    if(data.app.Callsign != null){
-                        //message.channel.send("Information ATC on " +obj.nameAirport);
-                        message.channel.send(obj.data.app.Callsign + " is open " + obj.data.app.Frequency + " Mhz ");
-                        message.channel.send("Atis : " + obj.data.app.Atis);
-                    }
-                }catch (e) {
-                    message.channel.send("is empty fot ICAO ex: &atc icao ");
-                }
-                try{
-                    if(data.twr.Callsign != null){
-                        //message.channel.send("Information ATC on " +obj.nameAirport);
-                        message.channel.send(obj.data.twr.Callsign + " is open " + obj.data.twr.Frequency + " Mhz ");
-                        message.channel.send("Atis : " + obj.data.twr.Atis);
-                    }
-                }catch (e) {
-
-                }
-                try{
-                    if(data.gnd.Callsign != null){
-                        //message.channel.send("Information ATC on " +obj.nameAirport);
-                        message.channel.send(obj.data.gnd.Callsign + " is open " + obj.data.gnd.Frequency + " Mhz ");
-                        message.channel.send("Atis : " + obj.data.gnd.Atis);
-                    }
-                }catch (e) {
-
-                }
-                try{
-                    if(data.del.Callsign != null){
-                        //message.channel.send("Information ATC on " +obj.nameAirport);
-                        message.channel.send(obj.data.del.Callsign + " is open " + obj.data.del.Frequency + " Mhz ");
-                        message.channel.send("Atis : " + obj.data.del.Atis);
-                    }
-                }catch (e) {
-                    console.log(e)
-                }
-                try {
-                    if (data.app.Callsign === null && obj.data.twr.Callsign === null && obj.data.gnd.Callsign === null && obj.data.del.Callsign === null){
-                        message.channel.send("No service contrôleur for the plateforme");
-                    }
-                }catch (error) {
-                    console.log(error)
-                }
-
-            })
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    if(command === "atc"){
+        let icao = args
+        if(!icao){
+            process.exit();
         }
+        let sr = process.env.apiatc+icao;
+        request(sr, function (error, response, body) {
+            const obj = JSON.parse(body);
+
+            if(obj.nameAirport != undefined){
+                message.channel.send("Information ATC on " +obj.nameAirport);
+            }
+
+            const data = obj.data
+            try {
+                if(data.app.Callsign != null){
+                    //message.channel.send("Information ATC on " +obj.nameAirport);
+                    message.channel.send(obj.data.app.Callsign + " is open " + obj.data.app.Frequency + " Mhz ");
+                    message.channel.send("Atis : " + obj.data.app.Atis);
+                }
+            }catch (e) {
+                message.channel.send("is empty fot ICAO ex: &atc icao ");
+            }
+            try{
+                if(data.twr.Callsign != null){
+                    //message.channel.send("Information ATC on " +obj.nameAirport);
+                    message.channel.send(obj.data.twr.Callsign + " is open " + obj.data.twr.Frequency + " Mhz ");
+                    message.channel.send("Atis : " + obj.data.twr.Atis);
+                }
+            }catch (e) {
+
+            }
+            try{
+                if(data.gnd.Callsign != null){
+                    //message.channel.send("Information ATC on " +obj.nameAirport);
+                    message.channel.send(obj.data.gnd.Callsign + " is open " + obj.data.gnd.Frequency + " Mhz ");
+                    message.channel.send("Atis : " + obj.data.gnd.Atis);
+                }
+            }catch (e) {
+
+            }
+            try{
+                if(data.del.Callsign != null){
+                    //message.channel.send("Information ATC on " +obj.nameAirport);
+                    message.channel.send(obj.data.del.Callsign + " is open " + obj.data.del.Frequency + " Mhz ");
+                    message.channel.send("Atis : " + obj.data.del.Atis);
+                }
+            }catch (e) {
+                console.log(e)
+            }
+            try {
+                if (data.app.Callsign === null && obj.data.twr.Callsign === null && obj.data.gnd.Callsign === null && obj.data.del.Callsign === null){
+                    message.channel.send("No service contrôleur for the plateforme");
+                }
+            }catch (error) {
+                console.log(error)
+            }
+
+        })
+    }
 })
 
 
@@ -183,8 +183,8 @@ client.on("message", message => {
     const command = args.shift().toLowerCase();
     if(command === "live"){
         if(args == false){
-           let args = "alexcaussades";
-           //console.log(args);
+            let args = "alexcaussades";
+            //console.log(args);
             fetch(configtwitch.data.url.channelsquery+args, {
                 method: "GET",
                 headers: {
@@ -193,45 +193,45 @@ client.on("message", message => {
                 }
             })
                 .then(res => res.json()).then(json =>{
-                    const id = json.data[0].id;
-                    const live = json.data[0].is_live;
-                    const avatar = json.data[0].thumbnail_url;
+                const id = json.data[0].id;
+                const live = json.data[0].is_live;
+                const avatar = json.data[0].thumbnail_url;
 
-                    if(live === false){
+                if(live === false){
+                    const exampleEmbed = new Discord.MessageEmbed()
+                        .setColor('#FF0000')
+                        .setTitle('Le Live n\'est pas actif actuellement.')
+                        .setAuthor(args)
+                        .setThumbnail(avatar)
+                        .setTimestamp()
+                        .setFooter('T.E.S.A');
+
+                    message.channel.send(exampleEmbed);
+                }else if(live !== false){
+
+                    fetch(configtwitch.data.url.broadcaster+id,{
+                        method: "GET",
+                        headers: {
+                            "client-id": configtwitch.data.auth.client_id,
+                            "Authorization": configtwitch.data.auth.bearer
+                        }
+                    }).then(res =>res.json()).then(json =>{
+                        const game = json.data[0].game_name;
+                        const title_stream = json.data[0].title;
                         const exampleEmbed = new Discord.MessageEmbed()
-                            .setColor('#FF0000')
-                            .setTitle('Le Live n\'est pas actif actuellement.')
+                            .setColor('#0099ff')
+                            .setTitle('Live ON')
+                            .setURL('https://www.twitch.tv/'+args)
                             .setAuthor(args)
+                            .setDescription(title_stream + " \n Game: "+ game)
                             .setThumbnail(avatar)
+                            .setImage(avatar)
                             .setTimestamp()
                             .setFooter('T.E.S.A');
 
                         message.channel.send(exampleEmbed);
-                    }else if(live !== false){
-
-                        fetch(configtwitch.data.url.broadcaster+id,{
-                            method: "GET",
-                            headers: {
-                                "client-id": configtwitch.data.auth.client_id,
-                                "Authorization": configtwitch.data.auth.bearer
-                            }
-                        }).then(res =>res.json()).then(json =>{
-                            const game = json.data[0].game_name;
-                            const title_stream = json.data[0].title;
-                            const exampleEmbed = new Discord.MessageEmbed()
-                                .setColor('#0099ff')
-                                .setTitle('Live ON')
-                                .setURL('https://www.twitch.tv/'+args)
-                                .setAuthor(args)
-                                .setDescription(title_stream + " \n Game: "+ game)
-                                .setThumbnail(avatar)
-                                .setImage(avatar)
-                                .setTimestamp()
-                                .setFooter('T.E.S.A');
-
-                            message.channel.send(exampleEmbed);
-                        })
-                    }
+                    })
+                }
             });
 
         }else if(args) {
