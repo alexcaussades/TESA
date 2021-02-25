03243af301ec474b2f36b0aefbaefe47f21d03c1
Alexcaussades <alexcaussades@gmail.com>
2021-02-25 22:20:03 +0100

    live-v2

---
+++ twitch/liveAutov2.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const configtwitch = require("./twitch.json");
+const bug = require("../bug")
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+
+module.exports.run = (client, args, pdo) => {
+
+
+}
